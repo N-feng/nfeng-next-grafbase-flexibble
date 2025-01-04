5b34a4c9de48742470c5a4d5cd1fb95cb32d4f7c
N-feng <308561157@qq.com>
2025-01-04 20:48:42 +0800

    fix🐛: fix some bug from grafbase.config

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -21,7 +21,7 @@ const Project = g.model('Project', {
   liveSiteUrl: g.url(), 
   githubUrl: g.url(), 
   category: g.string().search(),
-  createdBy: g.relation(() => User),
+  // createdBy: g.relation(() => User),
 }).auth((rules) => {
   rules.public().read()
   rules.private().create().delete().update()
